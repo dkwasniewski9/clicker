d33e7bfb4ed3f4f0ccae31be8676d7f3ac6c0553
dkwasniewski1999 <pingwin296@gmail.com>
2023-12-06 16:58:25 +0100

    adding dist

---
+++ dist/assets/index-NEw9fI9t.js
@@ -0,0 +1,10 @@
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();document.querySelector("#app").innerHTML=`
+  <div>
+    <h1>Sasin clicker</h1>
+    <p>Masz aktualnie <b id="balance"> 0</b> sasinow</p>
+    <p>Sasiny na sekunde: <b id="perSecond">0</b></p>
+    <div class="card">
+      <img src="/src/images/sasin.png" alt="banknot" onclick="counter()"> 
+    </div>
+  </div>
+`;setInterval(l,1e3);let o=1;function l(){let i=parseInt(document.getElementById("balance").innerHTML);document.getElementById("perSecond").innerHTML=(i/o).toFixed(2).toString(),++o,console.log(o)}
