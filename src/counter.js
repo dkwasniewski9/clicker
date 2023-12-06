6070beb5c5b78732160da6451bb4f4f921c464a1
dkwasniewski1999 <pingwin296@gmail.com>
2023-12-06 17:52:11 +0100

    fixing scripts implementation

--- src/counter.js
+++ src/counter.js
@@ -1,3 +1,5 @@
+const sasinImage = document.getElementById("img");
+sasinImage.addEventListener("click", counter);
 function counter() {
   let value = document.getElementById("balance").innerHTML;
   value = (parseInt(value) + 1).toString();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import "./style.css";
-import "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -7,7 +6,7 @@ document.querySelector("#app").innerHTML = `
     <p>Masz aktualnie <b id="balance"> 0</b> sasinow</p>
     <p>Sasiny na sekunde: <b id="perSecond">0</b></p>
     <div class="card">
-      <img src="https://a.allegroimg.com/s1024/0c5a72/3e8c5594495ca2773061d9c45e54" alt="banknot" onclick="counter()"> 
+      <img id="img" src="https://a.allegroimg.com/s1024/0c5a72/3e8c5594495ca2773061d9c45e54" alt="banknot"> 
     </div>
   </div>
 `;
