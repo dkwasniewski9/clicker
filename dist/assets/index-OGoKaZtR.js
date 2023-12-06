d3d2756f7623754007b136521c0f82bbe404e5b3
dkwasniewski1999 <pingwin296@gmail.com>
2023-12-06 17:24:56 +0100

    adding fixed dist

---
+++ dist/assets/index-OGoKaZtR.js
@@ -0,0 +1,10 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function c(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=c(e);fetch(e.href,r)}})();document.querySelector("#app").innerHTML=`
+  <div>
+    <h1>Sasin clicker</h1>
+    <p>Masz aktualnie <b id="balance"> 0</b> sasinow</p>
+    <p>Sasiny na sekunde: <b id="perSecond">0</b></p>
+    <div class="card">
+      <img src="https://a.allegroimg.com/s1024/0c5a72/3e8c5594495ca2773061d9c45e54" alt="banknot" onclick="counter()"> 
+    </div>
+  </div>
+`;
